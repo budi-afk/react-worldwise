4430cff6e921874cd3ddb4c7add00c5c28c8bf08
budi-afk <budiman1347@gmail.com>
2023-12-25 19:34:06 +0700

    implementing the city list

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,49 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import "./index.css";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:3001";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,19 @@
+import { NavLink } from "react-router-dom";
 import style from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={style.nav}>App Nav</nav>;
+  return (
+    <nav className={style.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">CITIES</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">COUNTRIES</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
