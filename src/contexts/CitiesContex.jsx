4334af556ce959b24176a957e97d8d1ae49f824c
budi-afk <budiman1347@gmail.com>
2024-01-02 08:18:07 +0700

    advanced state management system: context + useReducer

--- src/contexts/CitiesContex.jsx
+++ src/contexts/CitiesContex.jsx
@@ -1,46 +1,77 @@
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useEffect, useContext, useReducer } from "react";
 
 const CitiesContext = createContext();
 
 const BASE_URL = "http://localhost:3001";
 
+const initialState = { cities: [], isLoading: false, currentCity: {} };
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  function reducer(state, action) {
+    switch (action.type) {
+      case "loading":
+        return { ...state, isLoading: true };
+      case "rejected":
+        return { ...state, isLoading: false, error: action.payload };
+      case "cities/loaded":
+        return { ...state, cities: action.payload, isLoading: false };
+      case "city/loaded":
+        return { ...state, currentCity: action.payload, isLoading: false };
+      case "city/created":
+        return {
+          ...state,
+          cities: [...state.cities, action.payload],
+          isLoading: false,
+          currentCity: action.payload,
+        };
+      case "city/deleted":
+        return {
+          ...state,
+          cities: state.cities.filter((city) => city.id !== action.payload),
+          isLoading: false,
+          currentCity: {},
+        };
+    }
+  }
+
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
+    dispatch({ type: "loading" });
+
     async function fetchCities() {
       try {
-        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        dispatch({ type: "cities/loaded", payload: data });
       } catch {
-        alert("Error");
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "rejected", payload: "Failed to load the cities!" });
       }
     }
     fetchCities();
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
+    dispatch({ type: "loading" });
+
     try {
-      setIsLoading(true);
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "city/loaded", payload: data });
     } catch {
-      throw new Error("Failed to get city");
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "rejected", payload: "Failes to load the city!" });
     }
   }
 
   async function createCity(newCity) {
+    dispatch({ type: "loading" });
+
     try {
-      setIsLoading(true);
       const res = await fetch(`${BASE_URL}/cities`, {
         method: "POST",
         body: JSON.stringify(newCity),
@@ -50,17 +81,36 @@ function CitiesProvider({ children }) {
       });
 
       const data = await res.json();
-      setCities((cities) => [...cities, data]);
-    } catch (e) {
-      throw new Error("Failed to create City");
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "city/created", payload: data });
+    } catch {
+      dispatch({ type: "rejected", payload: "Failed to create the city" });
+    }
+  }
+
+  async function deleteCity(id) {
+    dispatch({ type: "loading" });
+
+    try {
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      dispatch({ type: "city/deleted", payload: id });
+    } catch {
+      dispatch({ type: "rejected", payload: "Failed to delete the city" });
     }
   }
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
